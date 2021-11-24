fc299658788710cfdc1bf3d3b67cf1651828d7c8
mulyadih98 <mulyadih1998@gmail.com>
2021-11-24 21:09:10 +0700

    add passport library

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function(Request $request){
     return $request->user();
 });
+
+Route::get('login', function() {
+    return 'Halaman Login';
+})->name('login');
+
+Route::middleware('auth:api')->get('/user/get', [UserController::class, 'get']);
+
